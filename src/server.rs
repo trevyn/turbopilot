6e1b27d057a74a50a2a8a56eb82d0dc1e2f5b4b0
trevyn <230691+trevyn@users.noreply.github.com>
2022-11-02 18:54:56 +0400

    add egui

--- src/server.rs
+++ src/server.rs
@@ -2,6 +2,7 @@
 #![allow(non_camel_case_types, non_snake_case)]
 #![cfg_attr(feature = "wasm", allow(dead_code))]
 
+use eframe::egui;
 use turbocharger::prelude::*;
 
 mod app;
@@ -9,5 +10,30 @@ mod app;
 #[tokio::main(flavor = "multi_thread", worker_threads = 10)]
 #[tracked]
 async fn main() -> Result<(), tracked::StringError> {
+	let native_options = eframe::NativeOptions::default();
+	eframe::run_native("turbopilot", native_options, Box::new(|cc| Box::new(MyEguiApp::new(cc))));
+
 	Ok(())
+}
+
+#[derive(Default)]
+struct MyEguiApp {}
+
+impl MyEguiApp {
+	fn new(cc: &eframe::CreationContext<'_>) -> Self {
+		// Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
+		// Restore app state using cc.storage (requires the "persistence" feature).
+		// Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
+		// for e.g. egui::PaintCallback.
+		Self::default()
+	}
+}
+
+impl eframe::App for MyEguiApp {
+	fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+		egui::CentralPanel::default().show(ctx, |ui| {
+			ui.heading("Hello World!");
+		});
+		ctx.request_repaint();
+	}
 }
